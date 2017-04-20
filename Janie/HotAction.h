15fda5e943f722c7acd34523a1511d40f980760c
This One <one@thiss-macbook-pro.local>
2017-04-19 21:01:47 -0600

    HotAction file

---
+++ Janie/HotAction.h
@@ -0,0 +1,23 @@
+//
+//  HotAction.h
+//  Janie
+//
+//  Created by This One on 4/18/17.
+//  Copyright © 2017 This One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class PercentageRect;
+
+
+@interface HotAction : UIView <UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) PercentageRect *percentageRect;
+@property (nonatomic, strong) NSString *shape;
+@property (nonatomic, weak) id target;
+@property (nonatomic) SEL action;
+
+- (id)initWithPercentageRect:(PercentageRect *)r action:(NSString *)act shape:(NSString *)shape;
+- (CGRect)desiredRectInView:(UIView *)parent;
+
+@end
